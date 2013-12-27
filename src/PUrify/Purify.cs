0baee0f6cf7c196d338b8d2cd1ddd91ccc51e3ce
Martijn Laarman <Mpdreamz@gmail.com>
2013-12-27 16:53:53 +0100

    Perform a noop on .net 4.5 and higher
    Perform a noop on .net 4.5 (and higher).

--- src/PUrify/Purify.cs
+++ src/PUrify/Purify.cs
@@ -4,27 +4,64 @@ using PUrify;
 
 namespace Purify
 {
-	public static class Purifier
-	{
+    public static class Purifier
+    {
 
-		private static bool isMono;
+        private static readonly bool hasBrokenDotNetUri;
 
-		static Purifier()
-		{
-		    isMono = typeof (Uri).GetField("m_Flags", BindingFlags.Instance | BindingFlags.NonPublic) == null;
-		}
+        private static readonly bool isMono;
+
+        static Purifier()
+        {
+            isMono = typeof(Uri).GetField("m_Flags", BindingFlags.Instance | BindingFlags.NonPublic) == null;
+            if (isMono)
+                return;
+
+            //ShouldUseLegacyV2 Quirks was introduced in .net 4.5
+            //Eventhough 4.5 is an inplace update of 4.0 this call will return 
+            //a different value if an application specifically targets 4.0 or 4.5+
+            var legacyV2Quirks = typeof(UriParser).GetProperty("ShouldUseLegacyV2Quirks", BindingFlags.Static | BindingFlags.NonPublic);
+            if (legacyV2Quirks == null)
+            {
+                hasBrokenDotNetUri = true; //neither 4.0 or 4.5
+                return;
+            }
+            var isBrokenUri = (bool)legacyV2Quirks.GetValue(null, null);
+            if (!isBrokenUri)
+                return; //application targets 4.5
+
+            //4.0 uses legacyV2quirks on the UriParser but you can set
+            //  <uri>
+            //    <schemeSettings>
+            //      <add name="http" genericUriParserOptions="DontUnescapePathDotsAndSlashes" />
+            //          </schemeSettings>
+            //  </uri>
+            //
+            //  this will fix AbsoluteUri but not ToString()
+            //  i.e new Uri("http://google.com/%2F").AbsoluteUri
+            //       will return the url untouched but:
+            //  new Uri("http://google.com/%2F").ToString()
+            //      will still return http://google.com//
+            //
+            //  so instead of using reflection perform a one off function test.
+
+            hasBrokenDotNetUri = !new Uri("http://google.com/%2F").ToString().EndsWith("%2F");
+
+        }
 
 
-		public static void Purify(this Uri uri)
-		{
-		    IPurifier purifier;
-		    if (isMono)
+
+        public static void Purify(this Uri uri)
+        {
+            IPurifier purifier = null;
+            if (isMono)
                 purifier = new PurifierMono();
-		    else
+            else if (hasBrokenDotNetUri)
                 purifier = new PurifierDotNet();
-
+            else return;
+            
             purifier.Purify(uri);
-		}
-	}
+        }
+    }
 }
 
