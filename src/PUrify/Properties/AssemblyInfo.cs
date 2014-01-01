4d26d6e874b034efd30bd01c11f8be0cd35f2450
Glenn Block <glenn.block@gmail.com>
2014-01-01 01:14:42 +0000

    Adding nuspec

--- src/PUrify/Properties/AssemblyInfo.cs
+++ src/PUrify/Properties/AssemblyInfo.cs
@@ -7,14 +7,14 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("glennblock")]
+[assembly: AssemblyProduct ("PUrify")]
+[assembly: AssemblyCopyright("Copyright 2013 Glenn Block")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.s
+[assembly: AssemblyVersion ("0.1.0")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
