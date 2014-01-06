7b90e1b153f2291610d86d2462beb28bae5bca01
Martijn Laarman <Mpdreamz@gmail.com>
2014-01-06 20:45:21 +0100

    started on WebRequest tests

--- Purify.Test.Clients.Net40/HttpWebRequestTests.cs
+++ Purify.Test.Clients.Net40/HttpWebRequestTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Reflection;
 using Should;
 using Xunit;
 
@@ -6,7 +8,29 @@ namespace Purify.Test.Clients.Net40
 {
     public class HttpWebRequestTests
     {
+        [Fact]
+        public void WeShouldRunTheseTestsUnderNET40()
+        {
+            var legacyV2Quirks = typeof(UriParser).GetProperty("ShouldUseLegacyV2Quirks", BindingFlags.Static | BindingFlags.NonPublic);
+            legacyV2Quirks.ShouldNotBeNull();
 
+            var isBrokenUri = (bool)legacyV2Quirks.GetValue(null, null);
+            isBrokenUri.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void TestSlash()
+        {
+            var uri = new Uri("http://localhost/%2F");
+            var webRequest = WebRequest.Create(uri);
+            webRequest.RequestUri.AbsoluteUri.EndsWith("/%2F").ShouldBeFalse();
+
+            uri = new Uri("http://localhost/%2F");
+            uri.Purify();
+            webRequest = WebRequest.Create(uri);
+            webRequest.RequestUri.AbsoluteUri.EndsWith("/%2F").ShouldBeFalse();
+        }
      
+
     }
 }
