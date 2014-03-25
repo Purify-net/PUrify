c53401953bfc35989c6aa6bfeb0bfce1ed56b6a9
Martijn Laarman <Mpdreamz@gmail.com>
2014-03-25 21:04:37 +0100

    Added additional tests testing joining a base uri with port not ending in a forward slash with a path only consisting of querystring

--- test/PUrify.Tests/UriInfoTest.cs
+++ test/PUrify.Tests/UriInfoTest.cs
@@ -96,6 +96,50 @@ namespace PUrify.Tests
         }
 
 
+        public class ConstructorWithOnlyQueryString_HostHasPort
+        {
+            private UriInfo _uriInfo;
+            private Uri _uri;
+
+            public ConstructorWithOnlyQueryString_HostHasPort()
+            {
+                var path = "?authToken=ABCDEFGHIJK";
+                var url = "http://localhost:80";
+                var uri = new Uri(url);
+                _uriInfo = new UriInfo(new Uri(uri, path).Purify(), url + path);
+                _uri = new Uri(uri, path).Purify();
+            }
+
+            [Fact]
+            public void ShouldStartPathWithAForwardSlash()
+            {
+                _uriInfo.Source.ShouldEqual("http://localhost:80/?authToken=ABCDEFGHIJK");
+            }
+
+            [Fact]
+            public void UriToStringShouldBeEqualToUriInfoSource()
+            {
+                _uri.ToString().ShouldEqual(_uriInfo.Source);
+            }
+
+            [Fact]
+            public void PortShouldNotBePartOfThePath()
+            {
+                _uri.AbsolutePath.ShouldEqual("/");
+            }
+
+            [Fact]
+            public void PathShouldContainQueryString()
+            {
+                _uri.PathAndQuery.ShouldEqual("/?authToken=ABCDEFGHIJK");
+            }  
+
+            [Fact]
+            public void QueryStringPropertyShouldNotBeBlank()
+            {
+                _uri.Query.ShouldEqual("?authToken=ABCDEFGHIJK");
+            }
+        }
         public class ConstructorWithOnlyQueryString
         {
             private UriInfo _uriInfo;
